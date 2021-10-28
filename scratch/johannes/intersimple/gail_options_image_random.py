e36aaef9f2f24b6ec19d1ca65ba4f3055c79d904
Johannes Fischer <johannes.fischer@kit.edu>
2021-10-28 18:16:17 +0200

    Move and add scratch scripts

--- scratch/johannes/intersimple/gail_options_image_random.py
+++ scratch/johannes/intersimple/gail_options_image_random.py
@@ -2,14 +2,13 @@
 import sys
 sys.path.append('../../../')
 
-from src.discriminator import CnnDiscriminator, CnnDiscriminatorFlatAction
+from src.discriminator import CnnDiscriminatorFlatAction
 from imitation.algorithms import adversarial
 import stable_baselines3
 import torch.utils.data
 import numpy as np
-from intersim.envs.intersimple import NRasterized, speed_reward
+from intersim.envs.intersimple import NRasterizedRandomAgent
 import itertools
-import functools
 from torch.distributions import Categorical
 import gym
 import torch
@@ -23,15 +22,14 @@ from tqdm import tqdm
 from src.policies.options import OptionsCnnPolicy
 from src.gail.options import OptionsEnv, LLOptions, HLOptions, RenderOptions
 from src.gail.train import train_discriminator, train_generator
-from src.evaluation.evaluation import Evaluation
 
-model_name = 'gail_options_image'
-env_settings = {'agent': 51, 'width': 36, 'height': 36, 'm_per_px': 2}
+model_name = 'gail_options_image_random'
+env_settings = {'width': 36, 'height': 36, 'm_per_px': 2}
 
-ALL_OPTIONS = [(v,t) for v in [0,2,4,6,8] for t in [5, 10]] # option 0 is safe fallback
+ALL_OPTIONS = [(v,t) for v in [0,2,4,6,8] for t in [5, 10, 20]] # option 0 is safe fallback
 
-def train(expert_data, epochs=20, expert_batch_size=32, generator_steps=1024, discount=0.99):
-    env = NRasterized(**env_settings)
+def train(expert_data, epochs=100, expert_batch_size=64, generator_steps=1024, discount=0.99):
+    env = NRasterizedRandomAgent(**env_settings)
     env.discount = discount
 
     tempdir = tempfile.TemporaryDirectory(prefix="quickstart")
@@ -39,7 +37,7 @@ def train(expert_data, epochs=20, expert_batch_size=32, generator_steps=1024, di
     logger.configure(tempdir_path / "GAIL/")
     print(f"All Tensorboards and logging are being written inside {tempdir_path}/.")
 
-    venv = make_vec_env(NRasterized, n_envs=1, env_kwargs=env_settings)
+    venv = make_vec_env(NRasterizedRandomAgent, n_envs=1, env_kwargs=env_settings)
     discriminator = adversarial.GAIL(
         expert_data=expert_data,
         expert_batch_size=expert_batch_size,
@@ -63,34 +61,31 @@ def train(expert_data, epochs=20, expert_batch_size=32, generator_steps=1024, di
         generator.tensorboard_log,
     )
 
-    for epoch in tqdm(range(epochs)):
+    for _ in tqdm(range(epochs)):
         train_discriminator(LLOptions(env, options=ALL_OPTIONS), generator, discriminator, num_samples=expert_batch_size)
         train_generator(HLOptions(env, options=ALL_OPTIONS), generator, discriminator, num_samples=generator_steps)
-
-        eval_env = env
-        ev = Evaluation(eval_env, n_eval_episodes=100)
-        ev.evaluate(epoch, generator, discriminator, expert_data)
+        generator.save(model_name)
     
     return generator
 
-# %%
-if __name__ == '__main__':
-    # %%
- 
-    with open("data/NormalizedIntersimpleExpertMu.001_NRasterizedInfoAgent51w36h36mppx2.pkl", "rb") as f:
-        trajectories = pickle.load(f)
-    transitions = rollout.flatten_trajectories(trajectories)
-    generator = train(transitions)
-
-    generator.save(model_name)
-
-    # %%
+def video(model_name, env):
     model = stable_baselines3.PPO.load(model_name)
-
-    env = RenderOptions(NRasterized(**env_settings), options=ALL_OPTIONS)
-
+    env = RenderOptions(env, options=ALL_OPTIONS)
     for s in env.sample_ll(model):
         if s['dones']:
             break
-
     env.close(filestr='render/'+model_name)
+
+def evaluate():
+    video(
+        model_name=model_name,
+        env=NRasterizedRandomAgent(**env_settings)
+    )
+
+# %%
+if __name__ == '__main__':
+
+    with open("../../../scratch/etienne/intersimple/data/NormalizedIntersimpleExpertMu.001N10000_NRasterizedRandomAgentw36h36mppx2.pkl", "rb") as f:
+        trajectories = pickle.load(f)
+    transitions = rollout.flatten_trajectories(trajectories)
+    train(transitions)
