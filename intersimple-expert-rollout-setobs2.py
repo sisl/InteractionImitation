4076b0361a96dae260eeec08752b3ce34f5b18a6
Arec Jamgochian <jamgochian95@gmail.com>
2022-03-03 19:37:29 -0800

    removed scratch except for expert data generator, which i moved to the top

---
+++ intersimple-expert-rollout-setobs2.py
@@ -0,0 +1,61 @@
+import torch
+import functools
+from src.core.sampling import rollout_sb3
+from intersim.envs import IntersimpleLidarFlatIncrementingAgent
+from intersim.envs.intersimple import speed_reward
+from intersim.expert import NormalizedIntersimpleExpert
+from src.util.wrappers import CollisionPenaltyWrapper, Setobs
+import numpy as np
+from gym.wrappers import TransformObservation
+
+obs_min = np.array([
+    [-1000, -1000, 0, -np.pi, -1e-1, 0.],
+    [0, -np.pi, -20, -20, -np.pi, -1e-1],
+    [0, -np.pi, -20, -20, -np.pi, -1e-1],
+    [0, -np.pi, -20, -20, -np.pi, -1e-1],
+    [0, -np.pi, -20, -20, -np.pi, -1e-1],
+    [0, -np.pi, -20, -20, -np.pi, -1e-1],
+]).reshape(-1)
+
+obs_max = np.array([
+    [1000, 1000, 20, np.pi, 1e-1, 0.],
+    [50, np.pi, 20, 20, np.pi, 1e-1],
+    [50, np.pi, 20, 20, np.pi, 1e-1],
+    [50, np.pi, 20, 20, np.pi, 1e-1],
+    [50, np.pi, 20, 20, np.pi, 1e-1],
+    [50, np.pi, 20, 20, np.pi, 1e-1],
+]).reshape(-1)
+
+def main(track:int, loc:int=0):
+    env = IntersimpleLidarFlatIncrementingAgent(
+        loc=loc,
+        track=track,
+        n_rays=5,
+        reward=functools.partial(
+            speed_reward,
+            collision_penalty=0
+        ),
+    )
+
+    policy = NormalizedIntersimpleExpert(env, mu=0.001)
+
+    env = Setobs(TransformObservation(
+        CollisionPenaltyWrapper(
+            env,
+            collision_distance=6, collision_penalty=100
+        ), lambda obs: (obs - obs_min) / (obs_max - obs_min + 1e-10)
+    ))
+    print(env.nv, 'vehicles')
+    expert_data = rollout_sb3(env, policy, n_episodes=150, max_steps_per_episode=200)
+
+    states, actions, rewards, dones = expert_data
+    print(f'Expert mean episode length {(~dones).sum() / states.shape[0]}')
+    print(f'Expert mean reward per episode {rewards[~dones].sum() / states.shape[0]}')
+    print(f'Observation mean', states[~dones].mean(0))
+    print(f'Observation std', states[~dones].std(0))
+
+    torch.save(expert_data, f'intersimple-expert-data-setobs2-loc{loc}-track{track}.pt')
+
+if __name__=='__main__':
+    import fire
+    fire.Fire(main)
--- scratch/etienne/trpo/sb3/sb3-ppo-pendulum.py
+++
@@ -1,6 +0,0 @@
-from stable_baselines3 import PPO
-from stable_baselines3.common.env_util import make_vec_env
-
-env = make_vec_env("Pendulum-v0", n_envs=4)
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=250000)
